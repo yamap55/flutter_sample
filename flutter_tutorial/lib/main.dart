fd5bd7d76b1916b9956a296485180e5a7b298228
yamap55 <tetuya120@hotmail.com>
2021-09-23 23:11:29 +0900

    [update] hello world -> random words

--- flutter_tutorial/lib/main.dart
+++ flutter_tutorial/lib/main.dart
@@ -3,20 +3,22 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Welcome to Flutter'),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        body: Center(
+          child: Text(wordPair.asPascalCase),
         ),
       ),
     );
