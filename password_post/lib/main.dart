914dfccb70d5682f2233569644838bf4547a1f16
yamap55 <1785817+yamap55@users.noreply.github.com>
2022-02-06 12:25:36 +0900

    [refactor] create list rows

--- password_post/lib/main.dart
+++ password_post/lib/main.dart
@@ -37,14 +37,18 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: ListView(
-        children: const [
-          ListTile(leading: Icon(Icons.vpn_key), title: Text('data1')),
-          Divider(thickness: 1.5),
-          ListTile(leading: Icon(Icons.vpn_key), title: Text('data2')),
-          Divider(thickness: 1.5),
-          ListTile(leading: Icon(Icons.vpn_key), title: Text('data3')),
-          Divider(thickness: 1.5),
-        ],
+        children: [
+          for (var i = 1; i <= 10; i++)
+            ((i) => [
+                  ListTile(
+                    leading: const Icon(Icons.vpn_key),
+                    title: Text('data$i'),
+                  ),
+                  const Divider(thickness: 1.5),
+                ])(i)
+        ].fold<List<StatelessWidget>>(
+          [], (prev, element) => [...prev, ...element]
+        )
       ),
       bottomNavigationBar: const Footer(),
     );
